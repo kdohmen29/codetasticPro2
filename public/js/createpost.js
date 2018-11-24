ce5f05af55098ae2e929ca72ea71545738b7d69d
Kyle Style <kdohmen29@gmail.com>
2018-11-24 12:39:38 -0800

    create post handlebars

---
+++ public/js/createpost.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+//make references to the input fields of creatpost.handlebars
+var postName = $("#create-post-input-name");
+var postDescription = $("#post-description-input");
+
+$(document).on("click", "#create-post-button", handleDepartmentFormSubmit);
+
+
+});
