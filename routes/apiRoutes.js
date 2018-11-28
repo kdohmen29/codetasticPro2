b8d8bfbbad1f78f1af36b5bdff656d83f764d9ff
Kyle Style <kdohmen29@gmail.com>
2018-11-27 19:30:02 -0800

    test heroku

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,7 +9,7 @@ module.exports = function (app) {
       res.json(dbDepartment);
     });
   });
-  
+
   app.get("/api/department/:id", function (req, res) {
     // Find one Author with the id in req.params.id and return them to the user with res.json
     db.Department.findOne({})
@@ -18,42 +18,40 @@ module.exports = function (app) {
 
 
 
-  
+
 
   app.get("/add/d", function (req, res) {
     // Create an Author with the data available to us in req.body
-    db.Department.create({name:"test"}).then(function(result){
+    db.Department.create({
+      name: "test"
+    }).then(function (result) {
       res.send(result)
 
     })
   });
-  
-  
- 
 
-     
-        app.get("/api/department/:id", function (req, res) {
-          // Find one Author with the id in req.params.id and return them to the user with res.json
-          db.Department.findOne({
-            where: {
-              id: req.params.id
-            }
-          }).then(function (dbDepartment) {
-            res.json(dbDepartment);
-          });
-        });
 
-        app.post("/api/department", function (req, res) {
-          // Create an Author with the data available to us in req.body
-          console.log(req.body);
-          db.Department.create(req.body).then(function (dbDepartment) {
-            res.json(dbDepartment);
 
-          });
-        });
 
+
+  app.get("/api/department/:id", function (req, res) {
+    // Find one Author with the id in req.params.id and return them to the user with res.json
+    db.Department.findOne({
+      where: {
+        id: req.params.id
       }
-    })
-  })
-};
+    }).then(function (dbDepartment) {
+      res.json(dbDepartment);
+    });
+  });
 
+  app.post("/api/department", function (req, res) {
+    // Create an Author with the data available to us in req.body
+    console.log(req.body);
+    db.Department.create(req.body).then(function (dbDepartment) {
+      res.json(dbDepartment);
+
+    });
+  });
+
+};
