a1b8d28ce9a48ff7e2e951d21ed1a84fc021b4d8
Kyle Style <kdohmen29@gmail.com>
2018-11-27 20:52:57 -0800

    working branch

--- public/js/department.js
+++ public/js/department.js
@@ -40,8 +40,12 @@ $(document).ready(function() {
       newTr.data("department", departmentData);
       newTr.append("<td>" + departmentData.text + "</td>");
       newTr.append("<td><a href='/api/departments" + departmentData.id + "'>Go to Posts</a></td>");
-      newTr.append("<td><a href='/createpost'>Create a Post</a></td>");
-      newTr.append("<td><a style='cursor:pointer;color:red' class='delete-department'>Delete Department</a></td>");
+
+
+      // These are for deleting and creating posts
+
+      // newTr.append("<td><a href='/createpost'>Create a Post</a></td>");
+      // newTr.append("<td><a style='cursor:pointer;color:red' class='delete-department'>Delete Department</a></td>");
       return newTr;
     }
   
